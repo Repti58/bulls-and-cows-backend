f5b59cbff8c7bbeff5920b8d32187cb69cee797d
repti85 <ids-reptile@bk.ru>
2022-06-23 17:57:57 +0300

    bd results

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const express = require('express');
 const cors = require('cors');
 
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('db.db');
+
+const users = ["Sara", "Mike", "James", "David", "Emily"];
+
+
+
+// function insertData(){
+//     var insertQuery = db.prepare("INSERT INTO mytable VALUES (?,?)");
+//     for (var i = 0; i < users.length; i++) {
+//         insertQuery.run(i, users[i]);
+//         console.log("Data inserted successfully...");
+//     }
+//     insertQuery.finalize();
+// }
+
+function accessData(){
+    db.each("SELECT * FROM mytable", function(err, results) {
+        // if (error) return console.log(err.message);
+        console.log(results);
+    });
+}
+
+// function deleteData(name){
+//     db.run("DELETE FROM mytable WHERE name=?",name, err =>{
+//         if (err) return console.log(err.message);
+//         console.log(`${name} deleted successfully...`);
+//     });
+// }
+
+db.serialize(function() {
+    // db.run("CREATE TABLE IF NOT EXISTS mytable (id, name)");
+    // insertData();
+    accessData();
+    // deleteData("James");
+});
+
+db.close();
+
 const PORT = process.env.port || 3002;
-const message = 'Здесь могла быть ваша реклама'
+const message = accessData();
 const app = express();
+
+
+
+
 app.use(cors());
 
 app.listen(PORT, () => {

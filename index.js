dd360eb378fca038382e30fa7f4b26b3cfd7676c
repti85 <ids-reptile@bk.ru>
2022-06-26 01:22:15 +0300

    insert steps to db

--- index.js
+++ index.js
@@ -5,24 +5,27 @@ const cors = require('cors');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('db.db');
 
-const users = ["Sara", "Mike", "James", "David", "Emily"];
+const steps = [5];
 
+const sql = 'INSERT INTO mytable (date, steps) VALUES(?,?)';
 
+function insertData(data) {
+    db.run(sql, ['26.02.2022', data]);
+    console.log("Data inserted successfully...");
+}
 
-// function insertData(){
-//     var insertQuery = db.prepare("INSERT INTO mytable VALUES (?,?)");
-//     for (var i = 0; i < users.length; i++) {
-//         insertQuery.run(i, users[i]);
-//         console.log("Data inserted successfully...");
-//     }
-//     insertQuery.finalize();
+// for (var i = 0; i < users.length; i++) {
+//     insertQuery.run(i, users[i]);
+// }
+// insertQuery.finalize();
 // }
 let message = ''
 function accessData() {
-    db.all("SELECT * FROM mytable", function(error, results) {
+    db.all("SELECT * FROM mytable", function (error, results) {
         if (error) return console.log(err.message);
-        console.log(results[0])
-        message = JSON.stringify(results)
+        console.log(results)
+        message = results
+
     });
 }
 
@@ -33,14 +36,14 @@ function accessData() {
 //     });
 // }
 
-db.serialize(function() {
-    // db.run("CREATE TABLE IF NOT EXISTS mytable (id, name)");
+db.serialize(function () {
+    // db.run("CREATE TABLE IF NOT EXISTS mytable (date, steps)");
     // insertData();
     accessData();
     // deleteData("James");
-});
+}); 
 
-db.close();
+// db.close();
 
 const PORT = process.env.port || 3002;
 // const message = accessData();
@@ -50,14 +53,21 @@ const app = express();
 
 
 app.use(cors());
+app.use(express.json())
 
 app.listen(PORT, () => {
-    console.log(`Server starting on port ${PORT}`)
+    console.log(`Server starting on port ${PORT}`) 
 })
 
 app.get('/api', (req, res) => {
-    
-    res.json({
-        message: message
-    })
+
+    res.json(message)
+})
+
+app.post('/api', (req, res) => {
+    data = req.body
+    console.log(data)
+    // res.send('Data inserted successfully...') 
+    res.status(201).json(data.number)
+    insertData(data.number)
 })

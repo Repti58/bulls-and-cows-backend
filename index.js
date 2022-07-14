124cc0fece1ddafbb66b5ad23d3f51da0665a840
repti85 <ids-reptile@bk.ru>
2022-07-15 00:38:54 +0300

    working game history and best results!!!

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ function insertData(data) {
     accessData();
 }
 
-let message = ''
-function accessData() {
 
+const accessData = async () => {
+const gameList = await new Promise((resolve, reject) => {
     db.all("SELECT * FROM mytable", function (error, results) {
-        if (error) return console.log(error.message);
-        message = results
+        if (error) return console.log(error.message);        
+        resolve(results)
     });
+})
+console.log(gameList);
+return gameList
 }
 
 
@@ -61,33 +64,39 @@ function accessData() {
 
 
 // let bestResults = [];
-
-const accessDataBestResults3 = new Promise((resolve, reject) => {
+const dbRequest = async () => {
+const accessDataBestResults3 = await new Promise((resolve, reject) => {
     db.all("SELECT * FROM mytable WHERE difficulty = 3 AND steps = (SELECT MIN(steps) FROM mytable WHERE difficulty = 3)", function (error, results) {
-        // if (error) return console.log(error.message);
+        // reject(() => console.log(error.message));
         let bestOf3 = results
+        console.log(bestOf3);
         resolve(bestOf3)
     })
 })
 
 
 // async function accessDataBestResults4() {
-const accessDataBestResults4 = new Promise((resolve, reject) => {
+const accessDataBestResults4 = await new Promise((resolve, reject) => {
     db.all("SELECT * FROM mytable WHERE difficulty = 4 AND steps = (SELECT MIN(steps) FROM mytable WHERE difficulty = 4)", function (error, results) {
         let bestOf4 = results
         // console.log(results[0]);
+        console.log(bestOf4);
         resolve(bestOf4)
     })
 })
 
-const accessDataBestResults5 = new Promise((resolve, reject) => {
+const accessDataBestResults5 = await new Promise((resolve, reject) => {
     db.all("SELECT * FROM mytable WHERE difficulty = 5 AND steps = (SELECT MIN(steps) FROM mytable WHERE difficulty = 5)", function (error, results) {
         let bestOf5 = results
         // console.log(results[0]);
+        console.log(bestOf5);
         resolve(bestOf5)
     })
 })
-
+const newArr = [...accessDataBestResults3, ...accessDataBestResults4, ...accessDataBestResults5]
+console.log(newArr);
+return (newArr)
+}
 // const accessDataBestResults = () => {
 //     // bestResults = []
 //     console.log(bestResults);
@@ -112,17 +121,18 @@ const accessDataBestResults5 = new Promise((resolve, reject) => {
 // }
 
 
-const accessDataBestResults = async() => {    
+// const accessDataBestResults = async() => {    
 
-    const data = await accessDataBestResults3;
-    // bestResults.push(data);
-    const data_1 = await accessDataBestResults4;
-    // bestResults.push(data_1);
-    const data_2 = await accessDataBestResults5;
-    const bestResults = [...data, ...data_1, ...data_2];
-    console.log(bestResults)
-    return bestResults
-}
+//     const data = await accessDataBestResults3;
+//     // bestResults.push(data);
+//     const data_1 = await accessDataBestResults4;
+//     // bestResults.push(data_1);
+//     const data_2 = await accessDataBestResults5;
+//     const bestResults = [...data, ...data_1, ...data_2];
+//     console.log(bestResults)
+//     console.log(accessDataBestResults3)
+//     return bestResults 
+// }
 
 
 db.serialize(() => {
@@ -130,7 +140,7 @@ db.serialize(() => {
     // insertData();
 
     accessData();
-    accessDataBestResults()
+    // accessDataBestResults()
 });
 
 // db.close();
@@ -148,13 +158,13 @@ app.listen(PORT, () => {
     console.log(`Server starting on port ${PORT}`)
 })
 
-app.get('/api', (req, res) => {
-    accessData();
-    res.json(message)
+app.get('/api', async(req, res) => {
+    const data = await accessData();
+    res.json(data)
 })
 
 
-app.get('/best_results', (req, res) => {
+app.get('/best_results', async(req, res) => {
     console.log('best results request');
     // bestResults = []
     // accessDataBestResults();
@@ -162,11 +172,9 @@ app.get('/best_results', (req, res) => {
     //     accessDataBestResults()
     //     resolve()
     // })
-    const result = async() => {
-        const data = await accessDataBestResults()
-        console.log(data);
-        return data
-    }
+    const result = await dbRequest()
+    //   console.log(result);  
+    // }
     res.json(result);
     // bestResults = [];
 
@@ -177,5 +185,5 @@ app.post('/api', (req, res) => {
     console.log(data);
     res.status(201).json(data.number);
     insertData(data);
-    accessDataBestResults();
+    // accessDataBestResults();
 })
